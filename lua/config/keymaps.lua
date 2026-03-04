a2867d32e37d965e222e2174fc22c1999ac5ed80
Dev Bilaspure <devbilaspure4941@gmail.com>
2026-03-04 16:25:52 +0530

    Add tmux vim-tmux-navigator plugging and related keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,8 @@
 -- Add any additional keymaps here
 --
 vim.opt.clipboard = "unnamedplus"
+
+vim.keymap.set("n", "c-k", ":wincmd k<CR>")
+vim.keymap.set("n", "c-j", ":wincmd j<CR>")
+vim.keymap.set("n", "c-h", ":wincmd h<CR>")
+vim.keymap.set("n", "c-l", ":wincmd l<CR>")
