be6403eab5ce0562b001753f8d59a5ca3edc4d6b
Dev Bilaspure <devbilaspure4941@gmail.com>
2026-03-04 11:23:42 +0530

    Add LSPs name in WhoIsSethDaniel/mason-tool-installer.nvim ensure_installed

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -130,7 +130,21 @@ return {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     opts = {
       ensure_installed = {
+        -- LSP
         "vtsls",
+        "html-lsp",
+        "css-lsp",
+        "jdtls",
+        "gopls",
+        "pyright",
+        "clangd",
+        "yaml-language-server",
+        "json-lsp",
+        "dockerfile-language-server",
+        "bash-language-server",
+        "marksman",
+
+        -- formatters
         "biome",
         "black",
         "clang-format",
